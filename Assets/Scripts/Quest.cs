72d27b360296b438991b28703cc88a6c0eb3a14f
jcobe-222 <yiwensun222@gmail.com>
2026-05-22 23:01:18 +0800

    初步进行了任务系统的制作

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool questAccepted=false;
+    public bool questCompleted=false;
+    public int targetEnemys = 3;
+    public int killedDeemys = 0;
+    public int rewardGold=100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/QuestNPC.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class QuestNPC : MonoBehaviour
+{
+    public Quest quest;
+    private bool playerInRange;
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))  //判断在范围之内且选择了与NPC进行交互
+        {
+            Talk();                                        
+        }
+    }
+    void Talk()
+    {
+        if (!quest.questAccepted)
+        {
+            quest.questAccepted = true;
+
+            Debug.Log("任务已接取：消灭3只史莱姆");
+        }
+        else if (!quest.questCompleted)
+        {
+            Debug.Log("任务还没完成");
+        }
+        else
+        {
+            Debug.Log("获得100金币奖励！");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
